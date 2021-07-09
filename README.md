6b446f50336d87f4b5e5522481655861c8cc8179
Nilaakash Singh <singhnilaakash@gmail.com>
2021-07-09 15:16:08 +0530

    Playing more with canvas

--- README.md
+++ README.md
@@ -18,3 +18,9 @@ Documentation: https://developer.apple.com/documentation/SwiftUI/AsyncImage
 ![alt text](https://github.com/NilaakashSingh/SwiftUI2021/blob/master/SwiftUI2021/FocusState/FocusState.png)
 
 Documentation: https://developer.apple.com/documentation/SwiftUI/FocusState
+
+## Canvas View
+
+![alt text](https://github.com/NilaakashSingh/SwiftUI2021/blob/master/SwiftUI2021/FocusState/FocusState.png)
+
+Documentation: https://developer.apple.com/documentation/SwiftUI/Canvas, https://developer.apple.com/documentation/swiftui/graphicscontext
--- SwiftUI2021/Canvas/CanvasView.swift
+++ SwiftUI2021/Canvas/CanvasView.swift
@@ -12,12 +12,23 @@ struct CanvasView: View {
     var body: some View {
         Canvas { context, size in
             context.stroke(
-                Path(ellipseIn: CGRect(origin: .zero, size: size)),
-                with: .color(.green),
-                lineWidth: 4)
+                Path(ellipseIn: CGRect(origin: CGPoint(x: 10, y: 10),
+                                       size: CGSize(width: size.width - 40,
+                                                    height: size.width - 40))),
+                with: .color(.black),
+                style: .init(lineWidth: 8, dashPhase: 2))
+            
+            
+            let halfSize = size.applying(CGAffineTransform(scaleX: 0.5, y: 0.5))
+            context.clip(to: Path(CGRect(origin: CGPoint(x: 10, y: 10),
+                                         size: halfSize)))
+            context.fill(
+                Path(ellipseIn: CGRect(origin: CGPoint(x: 7, y: 7), size: size)),
+                with: .color(.teal))
         }
-        .frame(width: 300, height: 200)
-        .border(Color.blue)
+        .navigationTitle("Canvas")
+        .navigationBarTitleDisplayMode(.inline)
+        .frame(width: 400, height: 400)
     }
 }
 
